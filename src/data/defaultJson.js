8b83a2f84f4ea8d290e747a96a3d84d2e2bbcca7
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-04-10 18:33:30 -0400

    add: resume default

---
+++ src/data/defaultJson.js
@@ -0,0 +1,33 @@
+export const resumeDefault = {
+  "personalInfo": {
+    "fullName": "",
+    "jobTitle": "",
+    "email": "",
+    "phone": "",
+    "location": "",
+    "linkedin": "",
+    "website": "",
+    "summary": ""
+  },
+  "education": [
+    {
+      "school": "",
+      "degree": "",
+      "location": "",
+      "startDate": "",
+      "endDate": "",
+      "gpa": "",
+      "courses": ""
+    }
+  ],
+  "experience": [
+  ],
+  "skills": [],
+  "projects": [
+    {
+      "title": "",
+      "description": "",
+      "link": ""
+    }
+  ]
+}
--- src/provider/resumeProvider.jsx
+++ src/provider/resumeProvider.jsx
@@ -1,8 +1,9 @@
 import { resumeContext } from "../context/resumeContext"
 import { useState } from "react";
+import { resumeDefault } from "../data/defaultJson";
 
 export default function ResumeProvider({children}) {
-    const [resumeData, setResumeData] = useState({})
+    const [resumeData, setResumeData] = useState(resumeDefault)
     
     return (
         <resumeContext.Provider value={{resumeData, setResumeData}}>
